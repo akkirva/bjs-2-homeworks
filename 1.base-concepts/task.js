e3aa9b289f15c504ad24e9f5d69f6b5365ce75d7
Your Name <you@example.com>
2022-08-11 16:59:14 +0300

    Calculation D

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,30 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+  let d = Math.pow(b, 2)-4*a*c;
+  let root1, root2;
+
+    if (d < 0) {
+      arr = [];
+    }
+    else if (d === 0) {
+      root1 = -b/(2*a);
+      arr = [root1];
+    }
+    else if (d > 0) {
+      root1 = (-b + Math.sqrt(d))/(2*a);
+      root2 = (-b - Math.sqrt(d))/(2*a);
+      arr = [root1, root2];
+    }
+
+    return arr;
+  }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
   // код для задачи №2 писать здесь
-
+  
   return totalAmount;
 }
