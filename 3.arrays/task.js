b5feef09c93fcaf1efa4402b0e7b9fa70cc99f9a
Your Name <you@example.com>
2022-09-18 00:26:54 +0300

    Arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,13 +1,13 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
+  let result = arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
+  
   // Ваш код
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter(n => n > 0).filter(n => n % 3 === 0).map(n => n * 10);
 
   // Ваш код
 
